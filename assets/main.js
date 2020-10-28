98da29122a4753f598c719a433dc526e2a3f828d
Ale <alessandrochb@gmail.com>
2020-10-28 16:41:24 +0100

    creato secondo dropdown menu + logica

--- assets/main.js
+++ assets/main.js
@@ -1,13 +1,22 @@
 // console.log('hello');
 $(document).ready(function(){
 
+// PRIMO DROPDOWN MENU
     $('.first-drop').mouseenter(function(){
-        $('.dropdown-container').toggle();
+        $('.dropdown-container-one').toggle();
 
     });
 
     $('.first-drop').mouseleave(function(){
-        $('.dropdown-container').toggle();
+        $('.dropdown-container-one').toggle();
+    });
+
+// SECONDO DROPDOWN MENU
+    $('.second-drop').mouseenter(function(){
+        $('.dropdown-container-two').toggle();
+    });
+    $('.second-drop').mouseleave(function(){
+        $('.dropdown-container-two').toggle();
     });
 
 
