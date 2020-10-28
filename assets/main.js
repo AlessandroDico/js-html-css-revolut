b9a807888f2a8b620f389a419a46870ca46c3d90
Ale <alessandrochb@gmail.com>
2020-10-28 16:08:31 +0100

    fatta logica primo dropdown con JS

--- assets/main.js
+++ assets/main.js
@@ -1,1 +1,23 @@
 // console.log('hello');
+$(document).ready(function(){
+
+    $('.first-drop').mouseenter(function(){
+        $('.drop-funzionalita').toggleClass('hidden visible');
+    });
+
+    $('.first-drop').mouseleave(function(){
+        $('.drop-funzionalita').toggleClass('hidden visible');
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+});
